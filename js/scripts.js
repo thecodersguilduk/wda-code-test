f976707d35703eca69c07ca3e45fcb70ca476560
Gibson <gibb.fernandes@gmail.com>
2018-08-30 14:10:59 +0100

    Added files

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+document.getElementById('items').innerHTML = "[3,7,'df','3',7,8,'df',23,6,2,67,'g','34',12,34,5]";
+
+function calculate() {
+  var numbersAndStrings = [3,7,'df','3',7,8,'df',23,6,2,67,'g','34',12,34,5];
+
+var integerTypeArray = numbersAndStrings.filter(number => !isNaN(number));
+
+var arrayLength = integerTypeArray.length;
+
+var sum = integerTypeArray.reduce((a, b) => a + parseInt(b), 0);
+
+var average = sum / arrayLength;
+
+  document.getElementById('result').innerHTML = average;
+
+console.log(integerTypeArray);
+console.log(sum);
+console.log(arrayLength);
+console.log(average);
+}
+
+function clear() {
+  document.getElementById('result').innerHTML = 'a';
+}
