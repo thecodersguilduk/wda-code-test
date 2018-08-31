beb77fbe4da921016636dc2fbf202d4904f6a597
Andrew <andy03@gmail.com>
2018-08-31 01:17:46 +0100

    Fixed Javascript indentation

--- js/script.js
+++ js/script.js
@@ -1,79 +1,92 @@
+/**
+ * Calculates the average of a set of numbers in a given array.
+ * @param DataArr An array containing data to be calculated
+ * @constructor
+ */
+
 function CalculateAverage(DataArr) {
 
-  this.DataArr = DataArr; // the input array
+    this.DataArr = DataArr; // the input array
 
-  this.FilteredArr; // array with all the invalid entrie removed
+    this.FilteredArr; // array with all the invalid entries removed
 
-  /* Filter anything that is not a number out of the array */
+    /**
+     * Filter anything that is not a number out of the array
+     * @constructor
+     */
 
-  this.FilterArr = function() {
+    this.FilterArr = function() {
 
-    var InputArrLength = this.DataArr.length;
+        var InputArrLength = this.DataArr.length;
 
-    var FilteredDataArr = this.DataArr;
+        var FilteredDataArr = this.DataArr;
 
-    for(var i = 0; i <= InputArrLength; i++) {
+        for(var i = 0; i <= InputArrLength; i++) {
 
-      if(isNaN(FilteredDataArr[i])) {
+            if(isNaN(FilteredDataArr[i])) {
 
-        FilteredDataArr.splice(i, 1); // not a number, remove the element from the array.
+                FilteredDataArr.splice(i, 1); // not a number, remove the element from the array.
 
-      }
+            }
 
-    }
+        }
 
     this.FilteredArr = FilteredDataArr;
 
-  }
+    };
 
-  function AddNums(total, num) {
+    /**
+     * Calculate the average sum of all the numbers in the array.
+     * @returns {string}
+     * @constructor
+     */
 
-    return total + parseInt(num);
+    this.CalculateAvg = function() {
 
-  }
+        try {
 
-  /* Calculate the average sum of all the numbers in the array. */
+            this.FilterArr();
 
-  this.CalculateAvg = function() {
+            var FilteredArrLength = this.FilteredArr.length;
 
-    try {
+            var total = this.FilteredArr.reduce(AddNums); // add the numbers together using the AddNums function
 
-      this.FilterArr();
+            var avg = total / FilteredArrLength;
 
-      var FilteredArrLength = this.FilteredArr.length;
+            return avg.toFixed(7);
 
-      var total = this.FilteredArr.reduce(AddNums); // add the numbers together using the AddNums function
+        } catch(e) { // catch any errors that man occur like an empty array
 
-      var avg = total / FilteredArrLength;
+            console.warn(e.message); // log waring to javascript console
 
-      return avg.toFixed(7);
+            return "An error occurred";
 
-    } catch(e) { // catch any errors that man occur like an empty array
+        }
 
-      console.warn(e.message); // log waring to javascript console
+    };
 
-      return "An error occurred";
+    function AddNums(total, num) {
+
+        return total + parseInt(num);
 
     }
-
-  }
 
 }
 
 window.onload = function() {
 
-  var OriginalDataArr = [3,7,'df','3',7,8,'df',23,6,2,67,'g','34',12,34,5];
+    var OriginalDataArr = [3,7,'df','3',7,8,'df',23,6,2,67,'g','34',12,34,5];
 
-  document.getElementById("InputArr").innerHTML = OriginalDataArr.toString();
+    document.getElementById("InputArr").innerHTML = OriginalDataArr.toString();
 
-  document.getElementById("CalcBtn").addEventListener("click", function(e) {
+    document.getElementById("CalcBtn").addEventListener("click", function(e) {
 
-    e.preventDefault();
+        e.preventDefault();
 
-    var AvgCalculator = new CalculateAverage(OriginalDataArr);
+        var AvgCalculator = new CalculateAverage(OriginalDataArr);
 
-    document.getElementById("CalcResponse").innerHTML = AvgCalculator.CalculateAvg();
+        document.getElementById("CalcResponse").innerHTML = AvgCalculator.CalculateAvg();
 
-  });
+    });
 
 };
