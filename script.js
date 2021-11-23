b4e61c0f4071855f705ca220970ba367e60446b7
Ross Berry <93287406+Ross-Berry@users.noreply.github.com>
2021-11-23 20:49:02 +0000

    Refactored working script

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
 window.onload = init;
 
 function init() {
-    function average() {
-        let alphanums = INPUT.join(",");
+    // Remove or convert non-integer items
+    function clean() {
+        let alphanums = input.join(",");
         let nums = alphanums
                     .replace(/[A-z]/g, "")
                     .split(",")
                     .filter(item => item !== "")
                     .map(item => parseInt(item));
+        
+        return average(nums);
+    };
+    // Calculate average for an array of integers
+    function average(nums) {
         let len = nums.length;
         let sum = nums.reduce((a, b) => a + b, 0);
         let ave = (sum / len).toFixed(7);
 
         return display(ave);
     };
-
+    // Update DOM element with passed in output
     function display(ave) {
         output.innerHTML = ave;
     };
 
-    const INPUT = [3,7,'df','3',7,8,'df',23,6,2,67,'g','34',12,34,5];
+    let input = [3,7,'df','3',7,8,'df',23,6,2,67,'g','34',12,34,5];
     let button = document.getElementById("calculate");
     let output = document.getElementById("output");
 
-    button.addEventListener("click", average);
+    button.onclick = clean;
 };
