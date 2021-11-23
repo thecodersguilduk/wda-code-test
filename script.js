323d0c0716c524804b01846eb82c711e455ff8f3
Ross Berry <93287406+Ross-Berry@users.noreply.github.com>
2021-11-23 11:39:35 +0000

    Built working script

---
+++ script.js
@@ -0,0 +1,21 @@
+window.onload = init;
+
+function init() {
+    const ARRAY = [3,7,'df','3',7,8,'df',23,6,2,67,'g','34',12,34,5];
+    let button = document.getElementById("calculate");
+    let output = document.getElementById("output");
+
+    button.addEventListener("click", function() {
+        let alphanums = ARRAY.join(",");
+        let nums = alphanums
+                    .replace(/[A-z]/g, "")
+                    .split(",")
+                    .filter(item => item !== "")
+                    .map(item => parseInt(item));
+        let len = nums.length;
+        let sum = nums.reduce((a, b) => a + b, 0);
+        let ave = (sum / len).toFixed(7);
+        
+        output.innerHTML = ave;
+    });
+};
