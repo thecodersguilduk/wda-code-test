f4cd5cf87ef007da714f6aa8569f0b92d48b57b5
Ross Berry <93287406+Ross-Berry@users.noreply.github.com>
2021-11-24 19:48:17 +0000

    Touched up and added comments

--- script.js
+++ script.js
@@ -1,33 +1,40 @@
 window.onload = init;
 
 function init() {
-    // Remove or convert non-integer items
-    function clean() {
-        let alphanums = input.join(",");
+    // Purify the array (remove or convert non-integer data types)
+    function purify() {
+        let alphanums = input.join(","); // Array to string conversion
         let nums = alphanums
-                    .replace(/[A-z]/g, "")
+                    // Search string for pattern and replace it 
+                    .replace(/[A-z]/g, "") 
+                    // Split string into an array of substrings
                     .split(",")
-                    .filter(item => item !== "")
+                    // Filter a new array filled with array items that pass the test function
+                    .filter(item => item !== "") 
+                    // Map a new array from calling a function on every array item
                     .map(item => parseInt(item));
         
         return average(nums);
     };
-    // Calculate average for an array of integers
+    // Work out the average
     function average(nums) {
         let len = nums.length;
+        // Calculate sum total of array items 
         let sum = nums.reduce((a, b) => a + b, 0);
-        let ave = (sum / len).toFixed(7);
+        // Calculate average of array items 
+        let ave = (sum / len).toFixed(7); // 7 decimal places
 
         return display(ave);
     };
-    // Update DOM element with passed in output
+    // Display the average
     function display(ave) {
-        output.innerHTML = ave;
+        // Update DOM element with final output
+        output.innerHTML = ave; 
     };
-
-    let input = [3,7,'df','3',7,8,'df',23,6,2,67,'g','34',12,34,5];
+    
+    let input = [3,7,'df','3',7,8,'df',23,6,2,67,'g','34',12,34,5]; // Provided input array
     let button = document.getElementById("calculate");
     let output = document.getElementById("output");
 
-    button.onclick = clean;
+    button.onclick = purify;
 };
